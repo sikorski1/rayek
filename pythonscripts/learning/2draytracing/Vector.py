9e6827f061744648768ad8f2b95d88e571a644e0
sikorski1 <kacpersikora.praca@gmail.com>
2025-01-22 21:57:18 +0100

    feat: new class Vector

---
+++ pythonscripts/learning/2draytracing/Vector.py
@@ -0,0 +1,8 @@
+class Vector:
+    def __init__(self, firstPoint, secondPoint):
+        self.A = firstPoint
+        self.B = secondPoint
+        self.length = self.calculateLength(self.A, self.B)
+        pass
+    def calculateLength(A, B):
+        pass
