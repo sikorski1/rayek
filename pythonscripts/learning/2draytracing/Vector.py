eb3dd27e358439a3c0fa0c4f353ca00b8e7287ba
sikorski1 <kacpersikora.praca@gmail.com>
2025-01-23 22:09:07 +0100

    feat: new class and vector length calculations added

--- pythonscripts/learning/2draytracing/Vector.py
+++ pythonscripts/learning/2draytracing/Vector.py
@@ -1,8 +1,10 @@
+from math import sqrt
 class Vector:
     def __init__(self, firstPoint, secondPoint):
         self.A = firstPoint
         self.B = secondPoint
         self.length = self.calculateLength(self.A, self.B)
         pass
-    def calculateLength(A, B):
-        pass
+    def calculateLength(self):
+        length = sqrt((self.A.x - self.B.x) ** 2 + (self.A.y - self.B.y) ** 2)
+        return length
