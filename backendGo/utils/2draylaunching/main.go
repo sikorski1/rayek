75f34cf59247c62373d4c84abd81e6df3582906c
sikorski1 <kacpersikora.praca@gmail.com>
2025-02-20 00:51:06 +0100

    feat: 2d raylaunching line of sight added

--- backendGo/utils/2draylaunching/main.go
+++ backendGo/utils/2draylaunching/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"image/png"
 	"math"
+	"math/cmplx"
 	"os"
 	"time"
 )
@@ -24,10 +25,10 @@ type RayLaunching struct {
 }
 
 func NewRayLaunching(matrixDimensions Point, tPos Point, tPower float64, tFreq float64, rFactor float64, wallPos []Vector) *RayLaunching {
-	step := 0.01
-	numberOfRays := 360
+	step := 0.1
+	numberOfRays := 720
 	numberOfInteracitons := 3
-	minimalPower := -70
+	minimalPower := -120.0
 	rows := int(matrixDimensions.Y*(1/step))+1
 	cols := int(matrixDimensions.X*(1/step))+1
 	Map := make([][]float64, rows)
@@ -52,15 +53,35 @@ func NewRayLaunching(matrixDimensions Point, tPos Point, tPower float64, tFreq f
 }
 
 func (rl *RayLaunching) calculateRayLaunching() {
-	for i := range(rl.NumberOfRays) {
-		iteration := 1
-		currInteractions := 0
+	maxSizeX := (float64(len(rl.Map[0]))-1)*rl.Step
+	maxSizeY := (float64(len(rl.Map))-1)*rl.Step
+	for i := range rl.NumberOfRays {
+		currInteracitons := 0
 		currPower := 0.0
-		dx, dy := math.Cos(float64(i)), math.Sin(float64(i))
-		for currInteractions <= rl.NumberOfInteracitons && currPower >= rl.MinimalPower {
-			x:=rl.TransmitterPos.X + dx * float64(iteration) //todo
-			y:=rl.TransmitterPos.Y + dy * float64(iteration) //todo
-			currPoint := Point{X:x,Y:y}
+		dx, dy := math.Cos(float64(i))*rl.Step, math.Sin(float64(i))*rl.Step
+		x, y := rl.TransmitterPos.X + dx, rl.TransmitterPos.Y + dy 
+		for (x >= 0 && x <= maxSizeX) && (y >= 0 && y <= maxSizeY) && currInteracitons <= rl.NumberOfInteracitons && currPower >= rl.MinimalPower {
+			xIdx := int(math.Round(x / rl.Step))
+			yIdx := int(math.Round(y / rl.Step))
+			
+			if rl.Map[yIdx][xIdx] == 1000 {
+				break
+			} else {
+				H := CalculateTransmittance(Point{X:rl.TransmitterPos.X,Y:rl.TransmitterPos.Y},Point{X:x,Y:y},rl.WaveLength,1.0)
+				currPower = 10*math.Log10(rl.TransmitterPower) + 20*math.Log10(cmplx.Abs(H))
+				if rl.Map[yIdx][xIdx] != 0 {
+					existingPowerLin := math.Pow(10, rl.Map[yIdx][xIdx]/10)
+					currPowerLin := math.Pow(10, currPower/10)
+					newPowerDb := 10 * math.Log10(existingPowerLin + currPowerLin)
+					rl.Map[yIdx][xIdx] = newPowerDb
+					
+				} else {
+					rl.Map[yIdx][xIdx] = currPower
+				}
+				x += dx
+				y += dy
+			}
+			
 		}
 	}
 }
@@ -109,11 +130,11 @@ func main() {
 	matrixDimensions := Point{X:40, Y:40}
 	transmitterPos := Point{X:5, Y:20}
 	transmitterPower := 5.0 
-	transmitterFreq := 3.4  
+	transmitterFreq := 1.4  
 	reflectionFactor := 0.7
 	walls := []Vector{{A:Point{X:0,Y:3}, B:Point{X:3,Y:6}}, {A:Point{X:1,Y:3}, B:Point{X:6,Y:3}}, {A:Point{X:6,Y:10}, B:Point{X:12,Y:12}},{A:Point{X:25,Y:10}, B:Point{X:25,Y:15}},{A:Point{X:5,Y:30}, B:Point{X:10,Y:35}},{A:Point{X:23,Y:36}, B:Point{X:25,Y:39}},{A:Point{X:1,Y:24}, B:Point{X:1,Y:26}},{A:Point{X:1,Y:28}, B:Point{X:1,Y:30}},{A:Point{X:1,Y:37}, B:Point{X:1,Y:40}},{A:Point{X:35,Y:36}, B:Point{X:30,Y:28}},{A:Point{X:40,Y:1}, B:Point{X:36,Y:2}},{A:Point{X:24,Y:3}, B:Point{X:25,Y:6}},{A:Point{X:16,Y:21}, B:Point{X:18,Y:22}},{A:Point{X:12,Y:18}, B:Point{X:12,Y:20}},{A:Point{X:18,Y:36}, B:Point{X:12,Y:36}}}
 	raylaunching := NewRayLaunching(matrixDimensions, transmitterPos, transmitterPower, transmitterFreq, reflectionFactor, walls)
-	fmt.Printf("%v \n", raylaunching.Map[32][66])
+	raylaunching.calculateRayLaunching()
 	stop := time.Since(start)
 	fmt.Printf("Computation time: %v \n", stop)
 	heatmap := GenerateHeatmap(raylaunching.Map)
