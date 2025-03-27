68576f7cc2ad9910136c8def64eda98207e69224
sikorski1 <kacpersikora.praca@gmail.com>
2025-03-27 22:09:20 +0100

    fix: structure changed

--- backendGo/utils/raylaunching/RayLaunching3D.go
+++ backendGo/utils/raylaunching/RayLaunching3D.go
@@ -9,6 +9,16 @@ type RayLaunching3DConfig struct {
 	step, reflFactor, stationPower, stationHeight, stationFreq, waveLength float64
 }
 
-func RayLaunching3D(matrix [][][]float64, wallNormals []Normal3D, config []RayLaunching3DConfig) [][][]float64 {
-	return matrix
+type RayLaunching3D struct {
+	PowerMap [][][]float64
+	WallNormals []Normal3D
+	Config []RayLaunching3DConfig
+}
+
+func NewRayLaunching3D(matrix [][][]float64, wallNormals []Normal3D, config []RayLaunching3DConfig) *RayLaunching3D {
+	return &RayLaunching3D{
+		PowerMap: matrix,
+		WallNormals: wallNormals,
+		Config: config,
+	}
 }
