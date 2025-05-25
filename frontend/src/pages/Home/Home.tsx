1fa2de70eb4dfaaaac6f6fbba89b51375ae7a4c5
sikorski1 <kacpersikora.praca@gmail.com>
2025-05-25 15:03:17 +0200

    feat: frontend update

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,13 +1,26 @@
-import Wrapper from "@/components/Wrapper/Wrapper";
+import { Wifi } from "lucide-react";
 import { Link } from "react-router-dom";
-import styles from "./home.module.scss"
+import styles from "./home.module.scss";
 export default function Home() {
 	return (
-		<Wrapper>
-			<div className={styles.box}>
-				<h1 className={styles.name}>RayCheck</h1>
-				<Link className={styles.link} to="/maps">Maps</Link>
+		<main className={styles.box}>
+			<div className={styles.container}>
+				<h1 className={styles.name}>Rayek</h1>
+				<div className={styles.buttonsBox}>
+					<Link className={styles.link} to="/maps">
+						What is RayLaunching?
+					</Link>
+					<Link className={styles.link} to="/maps">
+						What is RayTraycing?
+					</Link>
+					<Link className={styles.link} to="/maps">
+						Maps
+					</Link>
+				</div>
+				<div className={styles.iconBox}>
+					<Wifi className={styles.icon}></Wifi>
+				</div>
 			</div>
-		</Wrapper>
+		</main>
 	);
 }
